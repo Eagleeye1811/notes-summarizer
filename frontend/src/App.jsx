0fd2a6bf96434ec254cd79734c1991ab66902c61
prakhar362 <prakharshri2005@gmail.com>
2025-07-29 16:45:33 +0530

    removed app.jsx waste code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,11 @@ import Flashcards from './pages/Flashcards';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-100">
         <Routes>
           <Route path="/" element={<Subjects />} />
           <Route path="/subject/:subjectId" element={<SubjectDetail />} /> 
           <Route path="/flashcards/:subjectId" element={<Flashcards />} />
         </Routes>
-        
-      </div>
     </Router>
   );
 }
